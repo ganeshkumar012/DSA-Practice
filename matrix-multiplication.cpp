c4a4c0fb25a58e57acae1f4329463a6e75cf46bd
ganeshsaptang <ganeshkumar@saptanglabs.com>
2025-02-19 20:40:56 +0530

    program for matrix multiplication of 2D

---
+++ matrix-multiplication.cpp
@@ -0,0 +1,52 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void matrixMultiplication(vector<vector<int>> &m1,vector<vector<int>> &m2,vector<vector<int>> & res ){
+    
+    int r1 = m1.size();
+    int c1 = m1[0].size();
+    int r2 = m2.size();
+    int c2 = m2[0].size();
+    
+    
+    if(c1 != r2) {
+        cout << "invalid input" << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    res.resize(r1,vector<int>(c2,0));
+    
+    for(int i=0; i < r1; i++){
+        for(int j = 0;j<c2;j++ ){
+            for(int k = 0;k < c1;k++){
+                res[i][j] = m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    
+}
+
+int main() {
+   
+   vector<vector<int>>m1 = {{2,3},{1,5}};
+   vector<vector<int>>m2 = {{1,3},{1,5}};
+   
+   vector<vector<int>>res;
+   
+   matrixMultiplication(m1,m2,res);
+   
+   cout << "Multiplication of given two matrices is:\n";
+   
+   for(const auto &row : res){
+       for(int val:row){
+           cout << val << "\t";
+       }
+       
+       cout << endl;
+       
+   }
+    return 0;
+}
